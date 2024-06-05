2caabba6f0c4ad0fff21d26848754efe8d57465c
Vladis macbook <vladuxs1@gmail.com>
2024-06-05 18:55:23 +0300

    strapi nuxt module

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,23 @@
 <script setup>
-const runtimeConfig = useRuntimeConfig();
-const strapiUrl = runtimeConfig.public.strapiApiUrl;
-const { data } = await useFetch(`${strapiUrl}/api/websites?populate=*`);
-console.log({ data: data.value.data });
-const cards = data.value.data;
+const config = useRuntimeConfig();
+
+const { find } = useStrapi();
+const { data } = await find("websites", { populate: "*" });
 </script>
 
 <template>
-  <div>
-    <h1 class="my-8 text-red-200">Welcome to the homepage</h1>
-    <!-- <pre>
-    {{ cards[0].attributes.Image.data.attributes.url }}
-   </pre
-    > -->
-    <div class="grid grid-cols-3 gap-8">
-      <Card
-        v-for="card in cards"
-        :key="card.id"
-        :title="card.attributes.LinkTitle"
-        :price="card.attributes.Price"
-        :image="strapiUrl + card.attributes.Image.data.attributes.url"
-        :url="card.attributes.LinkUrl"
-      />
-    </div>
+  <div v-if="data" class="grid grid-cols-3 gap-8">
+    <Card
+      v-for="card in data"
+      :key="card.id"
+      :title="card.attributes.LinkTitle"
+      :price="card.attributes.Price"
+      :image="
+        card.attributes.Image
+          ? `${config.public.strapiApiUrl}${card.attributes.Image.data.attributes.url}`
+          : ''
+      "
+      :url="card.attributes.LinkUrl"
+    />
   </div>
 </template>
