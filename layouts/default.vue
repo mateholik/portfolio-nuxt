070aa504c5eb9d5c3a0c2eeebb89e65b05c14a0f
Vladis macbook <vladuxs1@gmail.com>
2024-06-24 00:33:10 +0300

    add toTop cta

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
+<script setup lang="ts">
+import { GoTop } from "@inotom/vue-go-top";
+</script>
+
 <template>
   <div>
     <Sidebar />
     <div class="container">
       <slot />
+      <go-top
+        :size="40"
+        bg-color="rgba(215, 117, 19, 0.9)"
+        :bottom="50"
+        :max-width="319"
+        :right="20"
+      ></go-top>
       <Copyright class="footer-bottom" />
     </div>
   </div>
