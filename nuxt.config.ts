bf83abdeeb8abeed955edb3adf3c18332764caaa
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-07-12 12:20:09 +0300

    refactor nuxt.config.ts for improved readability and consistency; update CSS and Strapi configuration formatting, and add SEO optimizations in app head settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/scss/main.scss"],
-  modules: [, "@nuxtjs/strapi", "nuxt-icons"],
+  css: ['~/assets/scss/main.scss'],
+  modules: ['@nuxtjs/strapi', 'nuxt-icons'],
+
   runtimeConfig: {
     public: {
-      strapiApiUrl: process.env.STRAPI_API_URL || "http://localhost:1337",
+      strapiApiUrl: process.env.STRAPI_API_URL || 'http://localhost:1337',
     },
   },
+
   strapi: {
-    url: process.env.STRAPI_API_URL || "http://localhost:1337",
-    prefix: "/api",
-    version: "v4",
+    url: process.env.STRAPI_API_URL || 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
+  },
+
+  // SEO and performance optimizations
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
   },
 });
