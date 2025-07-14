a8183277d4b5ebc07573f021c2fe7c180305b4ae
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-07-14 10:24:45 +0300

    add favicon, remove static imgs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
     },
   },
 });
