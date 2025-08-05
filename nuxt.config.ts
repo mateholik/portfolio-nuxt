27714e026946a462aa8bb19e455c71bbe4b5bccb
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-08-05 20:48:27 +0300

    add hotjar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -109,6 +109,19 @@ export default defineNuxtConfig({
       ],
       script: [
         {
+          innerHTML: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:1529920,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+          type: 'text/javascript',
+        },
+        {
           type: 'application/ld+json',
           innerHTML: JSON.stringify({
             '@context': 'https://schema.org',
